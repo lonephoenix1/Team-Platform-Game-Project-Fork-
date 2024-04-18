f727e9562f61adfbd1b485cdff24905ccfc7975a
DupciaBlada <wgarbowicz@edu.cdv.pl>
2024-04-18 12:49:48 +0200

    PodnoszenieMapaWiktorG

---
+++ Assets/ChangeTextOnTrigger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+public class ChangeTextOnTrigger : MonoBehaviour
+{
+    public TextMesh textMesh; // Referencja do komponentu Text Mesh Pro
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Sprawdzamy, czy obiekt, który wszed³ w trigger, ma tag "Player"
+        if (other.CompareTag("Player"))
+        {
+            // Zmieniamy tekst na nowy
+            textMesh.text = "Press E to pick up";
+        }
+    }
+}
---
+++ Assets/GoNaked.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GoNaked : MonoBehaviour
+{
+    public GameObject character; // Referencja do obiektu postaci
+    public GameObject[] elementsToDisable; // Tablica elementów do wy³¹czenia
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // SprawdŸ, czy obiekt w triggerze to postaæ
+        {
+            foreach (GameObject element in elementsToDisable)
+            {
+                element.SetActive(false); // Wy³¹cz ka¿dy element z tablicy
+            }
+        }
+    }
+}
+
